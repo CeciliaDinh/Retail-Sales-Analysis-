fb43f612cea36f4cad2615ce6dc973f39724de76
CeciliaDinh <dhang2003@gmail.com>
2024-05-28 23:38:29 +0700

    # remove duplicates

--- DataCleaning.sql
+++ DataCleaning.sql
@@ -1,6 +1,17 @@
+-- DATA CLEANING BY SQL--
+-- In this, there are 3 things that we're trying to do--
+-- Remove duplicates -- 
+-- Standardize format --
+-- remove outliers-- 
+-- save the cleaned data and plug it in PowerBI
 
 Select * from retailsales;
-DROP 
+-- we try best to not touch the original data because if we do, we take risk of losing valuable info --
+-- create a back up table-- 
+Create table sales_staging LIKE retailsales -- create table with the same structure as retailsales
+Insert into sales staging Select * from retailsales
+Select * from sales_staging
+
 -- remove duplicates
 WITH duplicates AS (
     SELECT *, 
@@ -11,7 +22,32 @@ WITH duplicates AS (
 SELECT *
 FROM duplicates
 WHERE row_num > 1;
--- we can see that all the duplicates have the same postal code= 44312, 87105--
--- let's retrieve +hem all-- 
-select * from retailsales
-where State= 'Ohio' AND Category='Furniture' AND City= 'Newark'
+-- create a backup table that holds row nums 
+CREATE TABLE sales_staging2 (
+    Ship_Mode TEXT, 
+    Segment TEXT, 
+    Country TEXT,
+    City TEXT, 
+    State TEXT, 
+    Postal_Code INT,
+    Region TEXT, 
+    Category TEXT,
+    Sub_Category TEXT, 
+    Sales DOUBLE, 
+    Quantity TINYINT(1),
+    Discount JSON,
+    Profit DOUBLE,
+    row_num INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO sales_staging2 SELECT *, 
+           ROW_NUMBER() OVER (
+               PARTITION BY City, State, Region, Category, 'Sub-Category', 'Postal Code', Discount, Profit, Sales, 'Ship Mode', Segment, Quantity) AS row_num
+    FROM sales_staging
+-- retrieve duplicates & check-- 
+SELECT * from sales_staging2 
+WHERE row_num>1
+-- before deleting those we have to make sure safe mode is off--
+SET SQL_SAFE_UPDATES = 0;
+DELETE 
+from sales_staging2 
+WHERE row_num>1
